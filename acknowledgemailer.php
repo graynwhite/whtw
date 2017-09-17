22162517fb788a766dd0d62774208c8964b36fa4
FJC on Toshiba 2015 <cauleyfrank@gmail.com>
2017-09-17 19:52:21 -0400

    Will consult with A2 about mailer

--- acknowledgemailer.php
+++ acknowledgemailer.php
@@ -1,24 +1,25 @@
 <?php
-		$testrun=isset($_GET[test])? $_GET[test] : 'no';
+		$testrun=isset($_RESPONSE['test'])? $_RESPONSE['test'] : 'no';
        //echo "arrived at mailer";
-	   $from_name=isset($_POST[pubname])? $_POST[pubname] : "publisher";
-	   $event_title= isset($_POST[title]) ? $_POST[title] : "Untitled Event";
-		$event_date= isset($_POST[fromDate]) ? $_POST[fromDate] : "1/1/2016";
-		$reserve_date=isset($_POST[reserve_date]) ? $_POST[reserve_date] : "1/1/2016";
+	   $from_name=isset($_POST['pubname'])? $_POST['pubname'] : "Newsletter publisher";
+	   $event_title= isset($_POST['title']) ? $_POST['title'] : "Untitled Event";
+		$event_date= isset($_POST['fromDate']) ? $_POST['fromDate'] : "1/1/2016";
+		$reserve_date=isset($_POST['reserve_date']) ? $_POST['reserve_date'] : "1/1/2016";
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-		$recipient = isset($_POST[recipient]) ? $_POST[recipient] : "cauleyfrank@gmail.com";
-        $recipient .=  ",cauleyfrank@gmail.com";
-		if($testrun == 'yes'){
-			$recipient ="cauleyfrank@gmail.com";
+		$recipient = isset($_POST['recipient']) ? $_POST['recipient'] : "cauleyfj@yahoo.com";
+        
+		if($testrun == 'yes')
+		{
+			$recipient ="cauleyfj@yahoo.com";
 		}
         // Set the email subject.
         $subject = "Your recent event information submission";
 
         // Build the email content.
-		$email_content = sprintf("To:  %s ,\n\n",$from_name);
-		if (isset($_POST[event_source])){
-			if ($_POST[event_source]=='gc'){
+		$email_content = sprintf("From:  %s ,\n\n",$from_name);
+		if (isset($_POST['event_source'])){
+			if ($_POST['event_source']=='gc'){
 				$email_content .= "We accessed your google calendar to find events occuring in the coming week. \n\n";
 			}
 		}
@@ -39,8 +40,10 @@
         // Build the email headers.
         $email_headers = "From: cauleyfrank@gmail.com";
 			
-		
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
+		echo('the recipient is '.$recipient);
+		echo("<br>The subject is". $subject);
+		echo("<br>Content is ".$email_content);
+        if (mail($recipient,$subject,$email_content)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
